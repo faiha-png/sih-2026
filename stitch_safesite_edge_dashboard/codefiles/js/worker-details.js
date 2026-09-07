4ce0abf1dff3623542e786cd639ce343ed4ae4fc
fizasketch <fiaashameer@gmail.com>
2026-09-07 11:45:07 +0530

    Add worker details JavaScript

---
+++ stitch_safesite_edge_dashboard/codefiles/js/worker-details.js
@@ -0,0 +1,9 @@
+const params = new URLSearchParams(window.location.search);
+const workerId = params.get("id");
+
+const worker = workers.find(worker => worker.id === workerId);
+
+if (worker) {
+    document.getElementById("worker-name").textContent =
+        `${worker.name} (Watch #${worker.watchId})`;
+}
